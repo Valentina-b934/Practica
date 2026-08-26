fa5f99431599ae2b520cde43924560860f97401b
Valentina-b934 <vblancor@uts.edu.co>
2026-08-26 17:21:11 -0500

    Delete backend directory

--- backend/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { myNotifications, markAsRead } = require('../controllers/notificationController');
-
-router.get('/', protect, myNotifications);
-router.put('/:id/read', protect, markAsRead);
-
-module.exports = router;
